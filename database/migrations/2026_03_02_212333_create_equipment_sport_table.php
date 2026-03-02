dbd28c7474f4dca3f545936ef825015f08745310
Pierre-Éloi Paré <pierreeloipare@gmail.com>
2026-03-02 16:28:35 -0500

    Refactor des migrations

---
+++ database/migrations/2026_03_02_212333_create_equipment_sport_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipment_sport', function (Blueprint $table) {
+            $table->foreignId('equipment_id')->constrained('equipment')->cascadeOnDelete();
+            $table->foreignId('sport_id')->constrained('sports')->cascadeOnDelete();
+            $table->primary(['equipment_id', 'sport_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipment_sport');
+    }
+};
