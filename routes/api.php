edd048c63e84c488577682c55e0a0e00a1bc44d0
Pierre-Éloi Paré <pierreeloipare@gmail.com>
2026-03-08 19:04:59 -0400

    Ajout des routes et correction des controlleurs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/equipments', [EquipmentController::class, 'index']);
+Route::get('/equipments/{id}', [EquipmentController::class, 'show']);
+Route::get('/equipments/{id}/popularity', [EquipmentController::class, 'popularity']);
+
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+
+Route::delete('/reviews/{id}', [ReviewController::class, 'destroy']);
+
+Route::get('/equipments/{id}/average-price', [EquipmentController::class, 'averagePrice']);
